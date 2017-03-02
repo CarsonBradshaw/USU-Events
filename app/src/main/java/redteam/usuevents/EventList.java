d5565a7182a923b2a7111f161cd62218db03c0e5
CarsonBradshaw <carsonabradshaw@aggiemail.usu.edu>
2017-03-01 19:47:05 -0700

    Added network connection to server from EventList for testing.

--- app/src/main/java/redteam/usuevents/EventList.java
+++ app/src/main/java/redteam/usuevents/EventList.java
@@ -1,6 +1,7 @@
 package redteam.usuevents;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Bundle;
@@ -12,6 +13,15 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+
 public class EventList extends Activity {
 
     @Override
@@ -20,7 +30,7 @@ public class EventList extends Activity {
         setContentView(R.layout.activity_event_list);
 
         //eventListTestArray integrating database
-        final String[] eventListTestArray={"Event 1","Event 5", "Event 4","Event 3","Event 4","Event 5"};
+        final String[] eventListTestArray={"Event 1","Event 2", "Event 3","Event 4","Event 5","Event 6"};
 
 
         ListAdapter eventAdapter = new CustomEventRow(this,eventListTestArray);
@@ -37,7 +47,56 @@ public class EventList extends Activity {
                 }
         );
 
+        new DatabaseJsonRetriever().execute("http://144.39.212.67/db_view.php");
+    }
 
+    public class DatabaseJsonRetriever extends AsyncTask<String, String, String>{
 
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if(reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.v("Result", result);
+        }
     }
 }
