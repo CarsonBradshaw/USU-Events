f8507281f3580436f8408bc3483de61c1d2f31b8
derek-garner <derek.garner@aggiemail.usu.edu>
2017-04-05 19:04:29 -0600

    Update appearence of event detail page

--- app/src/main/java/redteam/usuevents/EventDetailActivity.java
+++ app/src/main/java/redteam/usuevents/EventDetailActivity.java
@@ -3,6 +3,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,6 +24,7 @@ public class EventDetailActivity extends Activity{
     private int eventId;
     private TextView eventName;
     private TextView eventDescription;
+    private TextView eventAddress;
     private EventModel event;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +37,13 @@ public class EventDetailActivity extends Activity{
         event=(EventModel)intent.getSerializableExtra("EventModel");
         eventName.setText(event.getTitle());
         eventDescription.setText(event.getDescription());
-
+        eventAddress.setText(event.getAddress());
 
     }
     private void setUpUIViews(){
         eventName=(TextView)findViewById(R.id.eventName);
         eventDescription=(TextView)findViewById(R.id.eventDescription);
+        eventDescription.setMovementMethod(new ScrollingMovementMethod());
+        eventAddress=(TextView)findViewById(R.id.addressField_eventDetail);
     }
 }
