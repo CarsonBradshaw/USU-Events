ec085d529450e61d3361c10ee62b9e35e5fad52f
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-04-17 20:43:10 -0600

    Added Google Maps with marker to eventDetail page.

--- app/src/main/java/redteam/usuevents/EventDetailActivity.java
+++ app/src/main/java/redteam/usuevents/EventDetailActivity.java
@@ -6,6 +6,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +17,13 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.content.Intent;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
@@ -29,16 +37,16 @@ import java.util.Locale;
 
 import redteam.usuevents.models.EventModel;
 
-import static redteam.usuevents.R.id.imageView;
 
-
-public class EventDetailActivity extends Activity{
+public class EventDetailActivity extends AppCompatActivity implements OnMapReadyCallback{
     private int eventId;
     private TextView eventName;
     private TextView eventDescription;
-    private TextView eventAddress;
     private EventModel event;
-    private ImageView eventMap;
+    private String lat;
+    private String lng;
+    private String title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,27 +59,10 @@ public class EventDetailActivity extends Activity{
         eventName.setText(event.getTitle());
         eventDescription.setText(event.getDescription());
 
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
 
-//        List<Address> addresses  = null;
-//        try {
-//            addresses = geocoder.getFromLocation(Double.parseDouble(event.getLat()),Double.parseDouble(event.getLng()), 1);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-//        String city = addresses.get(0).getLocality();
-//        String state = addresses.get(0).getAdminArea();
-//        String country = addresses.get(0).getCountryName();
-//        String postalCode = addresses.get(0).getPostalCode();
-//
-//        eventAddress.setText(address + ", " + city + ", " + state + " " + postalCode);
-
-        String lat = event.getLat();
-        String lng = event.getLng();
-
-        Picasso.with(this).load("http://maps.google.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=17&size=2000x2000&sensor=false&markers=color:blue%7C"+lat+","+lng).into(eventMap);
+        lat = event.getLat();
+        lng = event.getLng();
+        title = event.getTitle();
 
         final Button interestButton = (Button) findViewById(R.id.interestButton_eventDetail);
         interestButton.setOnClickListener(new View.OnClickListener()
@@ -95,12 +86,23 @@ public class EventDetailActivity extends Activity{
             }
         });
 
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapEventDetail);
+        mapFragment.getMapAsync(this);
+
     }
     private void setUpUIViews(){
         eventName=(TextView)findViewById(R.id.eventName);
         eventDescription=(TextView)findViewById(R.id.eventDescription);
         eventDescription.setMovementMethod(new ScrollingMovementMethod());
-        eventAddress=(TextView)findViewById(R.id.addressField_eventDetail);
-        eventMap = (ImageView)findViewById(imageView);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng event = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+        googleMap.addMarker(new MarkerOptions().position(event)
+                .title(title)).showInfoWindow();
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(event,13));
     }
 }
