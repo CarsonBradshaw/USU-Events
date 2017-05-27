81b1329e3bc86cfa9fd9f69a22ecb0c5a0e9ed1a
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-05-27 16:45:18 -0600

    Initial commit on clean project.

---
+++ app/src/androidTest/java/io/github/carsonbradshaw/usu_events/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package io.github.carsonbradshaw.usu_events
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("io.github.carsonbradshaw.usu_events", appContext.packageName)
+    }
+}
