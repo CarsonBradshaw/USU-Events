310dd25113fde6911837e0b3e846eab457c028ea
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-06-02 23:56:20 -0600

    Started adding profile image. Need to right align and fix toolbar height.
    Look at caching image to prevent need for Picasso everytime we load the profile image.

--- app/src/main/java/redteam/usuevents/view/main/MainActivity.java
+++ app/src/main/java/redteam/usuevents/view/main/MainActivity.java
@@ -10,7 +10,9 @@ import android.util.Log;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
+import com.squareup.picasso.Picasso;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import redteam.usuevents.R;
 import redteam.usuevents.view.launchscreen.LaunchScreenActivity;
 import redteam.usuevents.view.login.LoginActivity;
@@ -28,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(LoginActivity.newIntent(MainActivity.this));
             finish();
         }
+
+        CircleImageView profileImage = (CircleImageView) findViewById(R.id.profile_image_button);
+        Picasso.with(this).load(auth.getCurrentUser().getPhotoUrl()).into(profileImage);
+
 
     }
 
