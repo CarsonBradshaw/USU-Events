63bf20e1681117cc19fbcf7e56f2301f4b17e409
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-06-12 22:01:42 -0600

    Refactored MainActivity. Prep for fragment switching.
    Make fragments singletons and only inflate views for fragments once.

--- app/src/main/java/redteam/usuevents/view/main/MainActivity.java
+++ app/src/main/java/redteam/usuevents/view/main/MainActivity.java
@@ -1,52 +1,87 @@
 package redteam.usuevents.view.main;
 
-import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.support.design.internal.BottomNavigationMenuView;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import redteam.usuevents.R;
-import redteam.usuevents.view.launchscreen.LaunchScreenActivity;
 import redteam.usuevents.view.login.LoginActivity;
 import redteam.usuevents.view.profile.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private Toolbar mToolbar;
+    private ImageView mFilterButton;
+    private ImageView mMapButton;
+    private CircleImageView mProfileImage;
+    private BottomNavigationView mBottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-        if (user == null) {
+        verifySignedInStatus();
+        bindViews();
+        setClickListeners();
+        loadProfileImage();
+
+    }
+
+    private void verifySignedInStatus(){
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
             // not signed in
             startActivity(LoginActivity.newIntent(MainActivity.this));
             finish();
         }
+    }
 
-        //profile image code - look at migrating most to viewmodel and create helper for loading images
-        final CircleImageView profileImage = (CircleImageView) findViewById(R.id.profile_image_button);
-        Glide.with(this).load(user.getPhotoUrl()).apply(RequestOptions.fitCenterTransform()).into(profileImage);
+    private void bindViews() {
+        mToolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        mFilterButton = (ImageView)findViewById(R.id.toolbar_filter);
+        mMapButton = (ImageView)findViewById(R.id.toolbar_map);
+        mProfileImage = (CircleImageView)findViewById(R.id.toolbar_profile_image);
+        mBottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation_view);
+    }
 
-        profileImage.setOnClickListener(new View.OnClickListener() {
+    private void setClickListeners(){
+
+        mFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
@@ -56,9 +91,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                switch (item.getItemId()){
+                    case R.id.bottom_nav_home:
 
+                        break;
+                    case R.id.bottom_nav_trending:
 
+                        break;
+                    case R.id.bottom_nav_subscriptions:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void loadProfileImage(){
+        //profile image code - look at migrating most to viewmodel and create helper for loading images
+        Glide.with(this).load(mFirebaseUser.getPhotoUrl()).apply(RequestOptions.fitCenterTransform()).into(mProfileImage);
     }
 
     public static Intent newIntent(Context context) {
---
+++ app/src/main/java/redteam/usuevents/view/main/MainHomeFragment.java
@@ -0,0 +1,11 @@
+package redteam.usuevents.view.main;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Admin on 6/12/2017.
+ */
+
+public class MainHomeFragment extends Fragment {
+
+}
