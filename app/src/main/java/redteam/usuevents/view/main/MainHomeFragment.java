6b52b000daadc1c57c15c82dc13c74c76b3c5f32
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-06-23 22:56:55 -0600

    Added recyclerView holder and adapter.
    Look at ways to abstract EventHolder and EventAdapter for reuse in MainTrendingFragment as well.

--- app/src/main/java/redteam/usuevents/view/main/MainHomeFragment.java
+++ app/src/main/java/redteam/usuevents/view/main/MainHomeFragment.java
@@ -7,8 +7,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import redteam.usuevents.R;
+import redteam.usuevents.model.Event;
 
 /**
  * Created by Admin on 6/12/2017.
@@ -19,6 +24,7 @@ public class MainHomeFragment extends Fragment {
     private static View sView;
 
     private RecyclerView mRecyclerView;
+    private EventAdapter mAdapter;
 
 
     public static MainHomeFragment getInstance() {
@@ -39,6 +45,49 @@ public class MainHomeFragment extends Fragment {
 
     private void bindViews(ViewGroup container){
         mRecyclerView = (RecyclerView)container.findViewById(R.id.fragment_main_home_recycler_view);
+    }
+
+
+
+
+
+
+    private class EventHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public void bind(Event event) {
+
+        }
+
+        public EventHolder(LayoutInflater inflater, ViewGroup parent, int layoutResourceId) {
+            super(inflater.inflate(layoutResourceId, parent, false));
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+
+
+
+    private class EventAdapter extends RecyclerView.Adapter<EventHolder> {
+
+        @Override
+        public EventHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(EventHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 
 }
