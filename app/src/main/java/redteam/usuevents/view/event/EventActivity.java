2e1ef1397763e16b7f50677518b1902bd5fc44fa
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-06-26 22:30:53 -0600

    Added map and event activities.
    Need to finish list_item_event and map to model before implementing recyclerView completely.

---
+++ app/src/main/java/redteam/usuevents/view/event/EventActivity.java
@@ -0,0 +1,11 @@
+package redteam.usuevents.view.event;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 6/26/2017.
+ */
+
+public class EventActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/redteam/usuevents/view/map/MapActivity.java
@@ -0,0 +1,11 @@
+package redteam.usuevents.view.map;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 6/26/2017.
+ */
+
+public class MapActivity extends AppCompatActivity {
+
+}
