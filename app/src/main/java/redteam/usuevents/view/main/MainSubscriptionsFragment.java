501fe458e3e6dceebb8949d4344a4048a5df2cb8
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-07-02 19:28:42 -0600

    Updated Subscriptions fragment with notification management.
    Need to embed manage notification view into recyclerView to prevent sticky header.

--- app/src/main/java/redteam/usuevents/view/main/MainSubscriptionsFragment.java
+++ app/src/main/java/redteam/usuevents/view/main/MainSubscriptionsFragment.java
@@ -1,14 +1,21 @@
 package redteam.usuevents.view.main;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import redteam.usuevents.R;
+import redteam.usuevents.adapter.EventsAdapter;
+import redteam.usuevents.model.Event;
 
 /**
  * Created by Admin on 6/14/2017.
@@ -33,6 +40,28 @@ public class MainSubscriptionsFragment extends Fragment {
         }
 
         bindViews();
+
+        //RecyclerView test code, remove once finalized
+        StaggeredGridLayoutManager manager;
+        if(getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            manager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        }else{
+            manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        }
+        mRecyclerView.setLayoutManager(manager);
+        List<Event> eventList = new ArrayList<Event>();
+        for(int i = 10; i<25; i++){
+            Event event = new Event();
+            event.setBeginDateTime("Friday, Aug "+i+" 6:00-7:30 PM");
+            event.setTitle("Aggie Football: USU vs New Mexico");
+            event.setLocation("Maverik Stadium, Logan, UT");
+            event.setImageUri("http://global.web.usu.edu/images/uploads/Carlos/Utah%20State%20University-%20early%20summer.jpg");
+            event.setNumberInterested(i);
+            eventList.add(event);
+        }
+        EventsAdapter eventAdapter = new EventsAdapter(eventList);
+        mRecyclerView.setAdapter(eventAdapter);
+        //Remove all above between comments when done figuring out configuration
 
         return sView;
     }
