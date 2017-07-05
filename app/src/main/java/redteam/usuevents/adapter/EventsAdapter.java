d2df3841903ac86b3878bb1f63cdbd37e4b0cec5
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-07-05 00:18:50 -0600

    Updated event item with more Trending decoration.
    Need to add similar interested count to Home and Subscriptions Fragments.

--- app/src/main/java/redteam/usuevents/adapter/EventsAdapter.java
+++ app/src/main/java/redteam/usuevents/adapter/EventsAdapter.java
@@ -1,6 +1,7 @@
 package redteam.usuevents.adapter;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -77,24 +78,30 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventHolde
         private TextView mLocation;
         private TextView mTime;
         private ImageView mImage;
+        private TextView mTitleUnderline;
+        private TextView mNumberInterested;
         private CardView mCardView;
 
         public void bind(Event event) {
             mEvent = event;
-            if(mTrending) setTrendingElements();
 
             mTitle.setText(mEvent.getTitle());
             mLocation.setText(mEvent.getLocation());
             mTime.setText(mEvent.getBeginDateTime());
+            if(mTrending){
+                mNumberInterested.setText(mEvent.getNumberInterested() + " Interested");
+            }
             //Glide has issues with loading on rotation from an abstracted adapter. Need to either migrate back into fragment or disallow rotation.
 //            Glide.with(itemView).load(mEvent.getImageUri()).apply(RequestOptions.fitCenterTransform()).into(mImage);
         }
 
-        public void setTrendingElements(){
+        public void applyTrendingStyles(){
             mCardView.setCardBackgroundColor(itemView.getResources().getColor(R.color.colorPrimary));
             mTitle.setTextColor(Color.WHITE);
             mLocation.setTextColor(Color.WHITE);
             mTime.setTextColor(Color.WHITE);
+            mTitleUnderline.setVisibility(View.VISIBLE);
+            mNumberInterested.setVisibility(View.VISIBLE);
         }
 
         public EventHolder(LayoutInflater inflater, ViewGroup parent, int layoutResourceId, boolean isTrending) {
@@ -105,7 +112,12 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventHolde
             mLocation = (TextView) itemView.findViewById(R.id.list_item_event_location);
             mTime = (TextView) itemView.findViewById(R.id.list_item_event_time);
             mImage = (ImageView) itemView.findViewById(R.id.list_item_event_image);
-            if(mTrending) mCardView = (CardView) itemView.findViewById(R.id.list_item_event_cardview);
+            if(mTrending) {
+                mCardView = (CardView) itemView.findViewById(R.id.list_item_event_cardview);
+                mTitleUnderline = (TextView) itemView.findViewById(R.id.list_item_event_trending_underline);
+                mNumberInterested = (TextView) itemView.findViewById(R.id.list_item_event_trending_number_interested);
+                applyTrendingStyles();
+            }
         }
 
         @Override
